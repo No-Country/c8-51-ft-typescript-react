45ea91acbb86b6eefcd6293410e75086b853d80e
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-11-14 11:36:27 -0300

    Se agrego funcion favoritos

--- Frontend/screens/HomeScreen.tsx
+++ Frontend/screens/HomeScreen.tsx
@@ -1,84 +1,95 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { Button, Searchbar, withTheme } from "react-native-paper";
+import {
+	Button,
+	Searchbar,
+	withTheme,
+	Surface,
+	IconButton,
+} from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Tab = createMaterialTopTabNavigator();
 // import BottomNav from "../components/BottomNav";
-const criptoJson = {
-	BTC: {
+const cryptoJson = [
+	{
 		name: "Bitcoin",
 		symbol: "BTC",
 		price: 15100.12,
-		change24h: 1.5,
+		change24h: 10.5,
+		fav: true,
 	},
-	ETH: {
+	{
 		name: "Ethereum",
 		symbol: "ETH",
 		price: 400.51,
 		change24h: -2.5,
+		fav: false,
 	},
-	LTC: {
+	{
 		name: "Litecoin",
 		symbol: "LTC",
 		price: 100,
 		change24h: 0.5,
+		fav: false,
 	},
-};
+];
 
 const AllCoinsComponent = (props) => {
 	const { colors } = props.theme;
+	const { items } = props;
 	return (
 		<View style={{ height: "100%", backgroundColor: colors.background }}>
-			{/* add headers */}
-			<View
+			<Surface
 				style={{
 					flexDirection: "row",
 					justifyContent: "space-between",
 					alignItems: "center",
 					padding: 10,
-					borderBottomWidth: 1,
-					borderBottomColor: colors.border,
+					backgroundColor: colors.background,
+					// borderBottomWidth: 1,
+					// borderBottomColor: colors.border,
 				}}
 			>
-				<Text style={{ color: colors.text }}>Name</Text>
-				<Text style={{ color: colors.text }}>Price</Text>
+				<Text style={{ color: colors.text, width: 50 }}>Name</Text>
+				<Text style={{ color: colors.text, width: 80 }}>Price</Text>
 				<Text style={{ color: colors.text }}>24h</Text>
-			</View>
-			{Object.keys(criptoJson).map((key) => {
+				<Text style={{ color: colors.text }}>Favs</Text>
+			</Surface>
+			{items.map((item) => {
 				{
-					var bg = criptoJson[key].change24h > 0 ? "green" : "red";
+					var bg = item.change24h > 0 ? "green" : "red";
 				}
 				return (
-					<View
+					<Surface
+						key={item.symbol}
 						style={{
 							flexDirection: "row",
 							justifyContent: "space-between",
 							alignItems: "center",
 							padding: 10,
-							borderBottomWidth: 1,
-							borderBottomColor: colors.border,
 						}}
 					>
-						<View style={{ flexDirection: "row", alignItems: "center" }}>
+						<View style={{ flex: 0, flexDirection: "column", width: 50 }}>
 							<Text style={{ color: colors.text, fontSize: 16 }}>
-								{criptoJson[key].symbol}/{" "}
-								<Text
-									style={{
-										color: colors.text,
-										fontSize: 8,
-										fontWeight: "normal",
-									}}
-								>
-									USDT
-								</Text>
+								{item.symbol}/{" "}
+							</Text>
+							<Text
+								style={{
+									color: colors.text,
+									fontSize: 8,
+									fontWeight: "normal",
+								}}
+							>
+								USDT
 							</Text>
 						</View>
-						<View style={{ flexDirection: "row", alignItems: "center" }}>
+						<View style={{ width: 80 }}>
 							<Text style={{ color: colors.text, fontSize: 16 }}>
-								${criptoJson[key].price}
+								${item.price}
 							</Text>
 						</View>
 						<View
@@ -87,14 +98,38 @@ const AllCoinsComponent = (props) => {
 								flexDirection: "row",
 								alignItems: "center",
 								backgroundColor: bg,
+								width: 60,
+								justifyContent: "center",
 							}}
 						>
-							<Text style={{ color: colors.text, fontSize: 16 }}>
+							<Text
+								style={{
+									color: colors.text,
+									fontSize: 12,
+								}}
+							>
 								{bg === "green" ? "+" : ""}
-								{criptoJson[key].change24h}%
+								{item.change24h}%
 							</Text>
 						</View>
-					</View>
+						<View
+							style={
+								{
+									// flex: 0,
+									// padding:0,
+									// alignSelf: "flex-end",
+								}
+							}
+						>
+							<MaterialCommunityIcons
+								name="star-outline"
+								// icon="star-outline"
+								// color={colors.text}
+								size={20}
+								onPress={() => console.log("Pressed")}
+							/>
+						</View>
+					</Surface>
 				);
 			})}
 		</View>
@@ -126,27 +161,24 @@ function Home(props) {
 				/>
 				<View style={{ height: "100%" }}>
 					<Tab.Navigator>
-						<Tab.Screen name="All" component={withTheme(AllCoinsComponent)} />
-						<Tab.Screen name="Favs" component={FavsCoinsComponent} />
+						<Tab.Screen name="All">
+							{() => (
+								<AllCoinsComponent items={cryptoJson} theme={props.theme} />
+							)}
+						</Tab.Screen>
+						<Tab.Screen name="Favs">
+							{() => (
+								<AllCoinsComponent
+									items={cryptoJson.filter((item) => item.fav)}
+									theme={props.theme}
+								/>
+							)}
+						</Tab.Screen>
 					</Tab.Navigator>
 				</View>
 			</SafeAreaView>
 		</>
 	);
-	// return (
-	// 	<View style={styles.container}>
-	// 		<Text>Home Screen</Text>
-	// 		<Button
-	// 			icon="camera"
-	// 			mode="contained"
-	// 			onPress={() => console.log("Pressed")}
-	// 		>
-	// 			Press me
-	// 		</Button>
-
-	// 		<StatusBar style="auto" />
-	// 	</View>
-	// );
 }
 
 const styles = StyleSheet.create({
