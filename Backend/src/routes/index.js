2b8970de87c31f9a5624cd583e44b8dba0d72a49
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-12-04 19:32:58 -0300

    Se agregó crud de favs

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const authRoutes = require('./auth.routes')
 const portfolioRoutes = require('./portfolio.routes')
+const favsRoutes = require('./favs.routes')
 const router = Router()
 
 router.use('/auth', authRoutes)
 router.use('/portfolio', portfolioRoutes)
+router.use('/favs', favsRoutes)
 
 module.exports = router
